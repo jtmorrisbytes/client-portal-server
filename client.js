515402a4a7659aaaf3f9812c2e0bb47c73c63763
= <=>
2020-04-27 09:24:25 -0500

    client script turns of ssl for ci

--- client.js
+++ client.js
@@ -14,18 +14,21 @@ async function main() {
   console.log(
     `connecting to postgres://${DATABASE_USERNAME}:****@${DATABASE_HOST}:${DATABASE_PORT}/${DATABASE_NAME}`
   );
-  let db = await massive({
+  let config = {
     host: DATABASE_HOST || "postgres",
     port: DATABASE_PORT || 5432,
     user: DATABASE_USERNAME || "postgres",
     database: DATABASE_NAME || "testdb",
     password: DATABASE_PASSWORD || "",
-    ssl: {
+    poolSize: 10,
+  };
+  if (!CI) {
+    config.ssl = {
       mode: "require",
       rejectUnauthorized: false,
-    },
-    poolSize: 10,
-  });
+    };
+  }
+  let db = await massive(config);
   console.log("running seed script");
   await db.seed(DATABASE_NAME);
   console.log("seed successful, exiting");
