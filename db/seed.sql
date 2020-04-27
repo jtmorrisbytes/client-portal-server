9a4256911aea2b18b424a16dc580d7094ec522c0
= <=>
2020-04-27 10:01:44 -0500

    trying again

--- db/seed.sql
+++ db/seed.sql
@@ -1,8 +1,7 @@
-CREATE DATABASE $1;
-
-create table setting (
-setting_id int primary key
-);
+-- create table setting (
+-- setting_id int primary key
+-- );
+drop table if exists users cascade;
 create table users (
   users_id serial primary key,
   email varchar(254) UNIQUE NOT NULL,
@@ -11,13 +10,13 @@ create table users (
   first_name text,
   last_name text,
   phone_number varchar(25),
-  setting_id int references setting(setting_id),
+  -- setting_id int references setting(setting_id),
   street_address text,
   city text,
   state text,
   zip text
 );
-
+drop table if exists client cascade;
 create table client (
   client_id serial Primary Key,
   email varchar(254) UNIQUE NOT NULL,
@@ -30,9 +29,11 @@ create table client (
   zip text
 );
 
-
+drop table if exists users_client cascade;
 create table users_client (
   users_client_id serial primary key,
   users_id int references users(users_id),
   client_id int references client(client_id)
 );
+
+insert into users(email, email_verified, hash) values('jthecybertinkerer@gmail.com', false, 'the hash');
