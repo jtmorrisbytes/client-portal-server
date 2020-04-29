397060207a870240da72dc122aec149414f9cc37
= <=>
2020-04-29 13:20:42 -0500

    release 0.0.2

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -1,5 +1,3 @@
-/// <reference types="@jtmorrisbytes/lib/User/Email" />
-
 import type { Request, Response, NextFunction } from "express";
 import { NIST } from "@jtmorrisbytes/lib/Nist";
 //until I find a way to make static classes
@@ -11,9 +9,9 @@ import EMAIL, { Email } from "@jtmorrisbytes/lib/User/Email";
 import PASSWORD from "@jtmorrisbytes/lib/User/Password";
 
 import { Resource } from "@jtmorrisbytes/lib/Resource";
-import { Name } from "@jtmorrisbytes/lib/User/Name"
+import { Name } from "@jtmorrisbytes/lib/User/Name";
 //TODO: allow reason to be passed in and create more specific messages for user
-import User from "@jtmorrisbytes/lib/User";
+import USER from "@jtmorrisbytes/lib/User";
 import ERROR from "@jtmorrisbytes/lib/Error";
 
 const MAX_ELAPSED_REQUEST_TIME = 60 * 1000 * 3;
@@ -24,69 +22,49 @@ const sha1 = require("sha1");
 const crypto = require("crypto");
 
 type NewUser = {
-  firstName?: Name,
-  lastName?:Name,
-  email: EMAIL.Email,
-  password: typeof PASSWORD.Password,
-  streetAddress?: string,
-  city?:string,
-  state?:string,
-  zip?:string
-}
-
+  firstName?: Name;
+  lastName?: Name;
+  email: EMAIL.Email;
+  password: PASSWORD.Password;
+  streetAddress?: string;
+  phoneNumber: string;
+  city?: string;
+  state?: string;
+  zip?: string;
+};
 
 async function register(req: Request, res: Response) {
   // try to destructure, respond with 500 if it fails
-  const db = req.app.get("db");
-  let userReq = ((req.body || {}).user) || {}
-  let newUser: NewUser = {
-    ...userReq,
-    firstName: new Name(userReq.firstName),
-    lastName: new Name(userReq.lastName),
-    email : new EMAIL.Email(userReq.email),
-    password: new PASSWORD.Password(userReq.password)
-    
-  }
-  let ErrRes:any = null;
-    if (newUser.email.value === null || undefined) {
-      res.status(400).json({
-        MESSAGE: "field email is required",
-        TYPE: "EMAIL_REQUIRED",
-      });
-      return;
+  try {
+    const db = req.app.get("db");
+    let userReq = (req.body || {}).user || {};
+    let newUser: NewUser = {
+      ...userReq,
+      firstName: new Name(userReq.firstName),
+      lastName: new Name(userReq.lastName),
+      email: new EMAIL.Email(userReq.email),
+      password: new PASSWORD.Password(userReq.password),
+    };
+    let errors: any[] = [];
+    if (newUser.email.value === null || newUser.email.isValid === false) {
+      errors.push(new EMAIL.EBadRequest());
     }
-    if (!newUser.isEmailValid()) {
-      res.status(400).json({ MESSAGE: `invalid email`, TYPE: "EMAIL_INVALID" });
-      return;
-    }
-    let dbResult = await db.user.getByEmail(email);
+    let dbResult = await db.user.getByEmail(newUser.email);
     if (dbResult.length > 0) {
-      res
-        .status(400)
-        .json({ MESSAGE: `email is already in use`, TYPE: "EMAIL_TAKEN" });
-      return;
+      errors.push(new EMAIL.ENotAuthorized());
     }
     // check password
-    if(newUser.password)
-    if (!password) {
-      res.status(400).json({
-        message: "field password is required",
-        TYPE: "PASSWORD_REQUIRED",
-      });
-      return;
+    if (newUser.password.value == null) {
+      errors.push(new PASSWORD.EMissing());
     }
     // email is marked as a unique, required field. if it already exists, the database will throw an error
     // run
-    if (newUser.isPasswordValid() === false) {
-      res.status(400).json({
-        reason: "Password is not valid",
-        TYPE: "PASSWORD_INVALID",
-      });
-      return;
+    if (newUser.password.isValid === false) {
+      errors.push(new PASSWORD.ENotValid());
     }
     if (process.env.NIST_TOKEN) {
       console.log("attempting NIST check with token ", process.env.NIST_TOKEN);
-      let nistHash = sha1(password);
+      let nistHash = sha1(newUser.password);
       let { found } = await axios.get(
         Nist.URL + nistHash + `?api_key=${process.env.NIST_TOKEN}`
       );
@@ -94,27 +72,30 @@ async function register(req: Request, res: Response) {
         console.log(
           "Password found in NIST Database, Refusing to allow password"
         );
-
-        res.status(400).json({
+        errors.push({
           ...new NIST.ENist(),
           link: Nist.INFOLINK,
         });
-        return;
       }
       console.log("Password not found in NIST Database. continuing");
     }
-    let encoded = Buffer.from(password).toString("base64");
+    if (errors.length > 0) {
+      res.status(400).json({ ...new Resource.EBadRequest(), errors: errors });
+      return;
+    }
+    // common errors have been handled at this point, continue
+    let encoded = Buffer.from(newUser.password).toString("base64");
     let hash = await bcrypt.hash(encoded, await bcrypt.genSalt(15));
     let result = await db.user.create(
-      firstName,
-      lastName,
+      newUser.firstName.value,
+      newUser.lastName.value,
       hash,
-      email,
-      phoneNumber,
-      streetAddress,
-      city,
-      state,
-      zip
+      newUser.email.value,
+      newUser.phoneNumber,
+      newUser.streetAddress,
+      newUser.city,
+      newUser.state,
+      newUser.zip
     );
     let user = result[0];
     console.log("/api/auth/register DB create user result", user);
@@ -122,16 +103,15 @@ async function register(req: Request, res: Response) {
       id: user.users_id,
     };
     res.json({ session: req.session });
-    
   } catch (e) {
     process.stdout.write("Failed to register user ");
     let errRes = new Resource.EGeneralFailure();
     if (e instanceof SyntaxError) {
       process.stdout.write("because of a syntax error ");
-      errRes = {...errRes,...new ERROR.Error.ESyntaxError()};
+      errRes = { ...errRes, ...new ERROR.Error.ESyntaxError() };
     } else if (e instanceof TypeError) {
       process.stdout.write("because of a Type Error ");
-      errRes = {...errRes,...new ERROR.Error.ETypeError()};
+      errRes = { ...errRes, ...new ERROR.Error.ETypeError() };
     }
     res.status(500).json(errRes);
     process.stdout.write("with stacktrace:\n");
@@ -152,48 +132,43 @@ async function logIn(req: Request, res: Response) {
     req.body.user || "NOT FOUND"
   );
   try {
+    let errors = [];
     let { email, password } = req.body.user;
     if (!email) {
       console.warn("/api/auth/login: email was missing from request");
       //Ideally allow reason to be passed through
-      const reason = "email was missing from request";
-      res.status(400).json(new Resource.EBadRequest());
-    } else if (!password) {
+      errors.push(new EMAIL.EMissing());
+    }
+    if (!password) {
       console.warn("/api/auth/login: password was missing from request");
-      const reason = "password was missing from request";
-      res.status(400).json(new Resource.EBadRequest());
+      errors.push(new PASSWORD.EMissing());
+    }
+    if (errors.length > 0) {
+      res.status(400).json({ ...new Resource.EBadRequest(), errors });
+      return;
     } else {
-      console.log("searching database for username");
-      let result = await req.app.get("db").user.getByEmail(email);
-      if (result.length === 0) {
-        console.warn(
-          `/api/auth/login: user '${email.substr(
-            0,
-            email.indexOf("@") - 2
-          )}' not found`
-        );
-        res.status(401).json(new Resource.ENotFound());
+      errors = [];
+    }
+    console.log("searching database for username");
+    let result = await req.app.get("db").user.getByEmail(email);
+    if (result.length === 0) {
+      res.status(401).json(new USER.ENotFoundByEmail());
+    } else {
+      let user = result[0];
+      console.log("/api/auth/login user found, comparing hash");
+      let authenticated: Boolean = await bcrypt.compare(
+        Buffer.from(password).toString("base64"),
+        user.hash
+      );
+      if (authenticated) {
+        console.log("logging in user with id:", user.users_id);
+        (req.session.user = {
+          id: user.users_id,
+        }),
+          res.json({ session: req.session });
       } else {
-        let user = result[0];
-        console.log("/api/auth/login user found, comparing hash");
-        let authenticated: Boolean = await bcrypt.compare(
-          Buffer.from(password).toString("base64"),
-          user.hash
-        );
-        if (authenticated) {
-          console.log("logging in user with id:", user.users_id);
-          (req.session.user = {
-            id: user.users_id,
-          }),
-            res.json({ session: req.session });
-        } else {
-          console.warn("/api/auth/login recieved an invalid password");
-          res.status(401).json({
-            ...new Resource.ENotAuthorized(),
-            REASON: "That password is incorrect",
-            TYPE: "PASSWORD_INCORRECT",
-          });
-        }
+        console.warn("/api/auth/login recieved an invalid password");
+        res.status(401).json(new PASSWORD.ENotAuthorized());
       }
     }
   } catch (e) {
@@ -204,15 +179,15 @@ async function logIn(req: Request, res: Response) {
     };
     if (e instanceof TypeError) {
       errRes = {
-        ...new Error.ETypeError(),
-        REASON: e,
+        ...errRes,
+        ...new ERROR.Error.ETypeError(),
       };
     }
     if (e instanceof ReferenceError) {
       process.stdout.write("because of a ReferenceError");
       errRes = {
-        ...new Error.ESyntaxError(),
-        REASON: e,
+        ...errRes,
+        ...new ERROR.Error.ESyntaxError(),
       };
     }
     process.stdout.write(" with stacktrace\n");
@@ -223,9 +198,9 @@ async function logIn(req: Request, res: Response) {
 async function logOut(req: Request, res: Response) {
   req.session.destroy((err) => {
     if (err) {
-      console.log();
-      res.clearCookie("connect.sid");
+      console.log(err);
     }
+    res.clearCookie("connect.sid");
   });
   res.sendStatus(200);
 }
