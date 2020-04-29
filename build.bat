a43b063af0d252cbc21b79b1bec935ea1e1ad8e0
= <=>
2020-04-29 01:39:30 -0500

    prepare to update server to use jtmorrisbytes/lib

--- build.bat
+++ build.bat
@@ -1,7 +1,5 @@
-yarn install
-rmdir /s /q dist
-yarn webpack --mode production
-copy src\package.json .\dist
-copy src\package-lock.json .\dist
-copy src\client.js .\dist
-copy src\db.ca-certificate.pem
+
+copy .\src\package.json .\dist
+copy .\src\yarn.lock .\dist
+copy .\src\client.js .\dist
+copy .\src\db.ca-certificate.crt
