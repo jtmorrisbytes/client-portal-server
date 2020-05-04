5a145ecbc8dc5aea6815202346ea8df44966c2e8
= <=>
2020-05-03 23:31:13 -0500

    updated db.user.create to return city state and zip code

--- src/db/user/create.sql
+++ src/db/user/create.sql
@@ -1,2 +1,2 @@
 INSERT into users (first_name, last_name, hash, email, phone_number, street_address, city, state, zip)
-values ($1,$2,$3,$4,$5,$6,$7, $8, $9) returning users_id,first_name, last_name, email, phone_number;
+values ($1,$2,$3,$4,$5,$6,$7, $8, $9) returning users_id,first_name, last_name,phone_number, email, city,state,zip ;
