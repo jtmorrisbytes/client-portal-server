f2b9f9bedf5394b7d1cd343f815c3368725be906
= <=>
2020-05-04 14:52:34 -0500

    fixed wrong path in routes/user for getUser

--- src/routes/user.js
+++ src/routes/user.js
@@ -2,6 +2,6 @@ const controller = require("../controllers/user");
 const router = require("express").Router();
 const basePath = "/user";
 // get /user with no parameter getts the currently logged in user
-router.get(basePath, controller.getUser);
+router.get("/", controller.getUser);
 
 module.exports = { basePath, router };
